f73cce22100d3046297d3ac4d8eb6bd01a72dd0c
nitishplus98 <nitishplus98@gmail.com>
2018-04-22 18:51:15 +0530

    Add basic implementation

--- src/main.py
+++ src/main.py
@@ -4,12 +4,11 @@ import pandas as pd
 import math
 
 n_features=10
-
 n_hidden=10
 
 def nn_model():
 
-    x = tf.placeholder(tf.float32,shape=(None,n_features))
+    x = tf.placeholder(tf.float32,shape=(None,n_features)) #Specifies shape of the tensor
     y = tf.placeholder(tf.float32,shape=(None,1))
 
     w1=tf.Variable(tf.random_normal([n_features,n_hidden]))
@@ -67,46 +66,46 @@ def main():
     x,y,pred,loss = nn_model()
     optimizer = tf.train.AdamOptimizer(0.5).minimize(loss)
 
-    session = tf.InteractiveSession()
-    tf.global_variables_initializer().run()
-    for ep in range(epochs):
+    with tf.Session() as session:
+        tf.global_variables_initializer().run()
+        for ep in range(epochs):
 
-        """
-        To segregate data for training the network and data for validation
-        """
-        np.random.shuffle(train_data)
-        vdata_size = math.floor(validation_tr_fraction*len(train_data))
-        vdata = train_data[:vdata_size]
-        train_data = train_data[vdata_size+1:]
-        train_data_size = len(train_data)
+            """
+            To segregate data for training the network and data for validation
+            """
+            np.random.shuffle(train_data)
+            vdata_size = math.floor(validation_tr_fraction*len(train_data))
+            vdata = train_data[:vdata_size]
+            train_data = train_data[vdata_size+1:]
+            train_data_size = len(train_data)
 
 
-        no_of_batches = math.ceil(train_data_size/batch_size)
-        batch_no = 1
-        batch_x = np.zeros(shape=(batch_size,n_features), dtype = np.float32)
-        batch_y = np.zeros(shape=(batch_size,1), dtype = np.float32)
-        for i in range(train_data_size):
-            if batch_no > no_of_batches:
-                break
+            no_of_batches = math.ceil(train_data_size/batch_size)
+            batch_no = 1
+            batch_x = np.zeros(shape=(batch_size,n_features), dtype = np.float32)
+            batch_y = np.zeros(shape=(batch_size,1), dtype = np.float32)
+            for i in range(train_data_size):
+                if batch_no > no_of_batches:
+                    break
+                
+                if ( i % batch_size == 0 ) and ( i != 0 ):
+                    loss_val,_ = session.run([loss,optimizer],feed_dict={x:batch_x,y:batch_y})
+                    batch_no = batch_no + 1
+                else:
+                    batch_x[i%batch_size] = train_data[i][:-1]
+                    batch_y[i%batch_size] = train_data[i][-1]
             
-            if ( i % batch_size == 0 ) and ( i != 0 ):
-                loss_val,_ = session.run([loss,optimizer],feed_dict={x:batch_x,y:batch_y})
-                batch_no = batch_no + 1
-            else:
-                batch_x[i%batch_size] = train_data[i][:-1]
-                batch_y[i%batch_size] = train_data[i][-1]
-
-        if i % batch_size != 0:
-            lbatch_x = batch_x[:i]
-            lbatch_y = batch_y[:i]
-            loss_val,_ = session.run([loss,optimizer],feed_dict={x:lbatch_x,y:lbatch_y})
+            
+            print(vdata[:,:-1].shape,vdata[:,-1].shape)
+            vloss,vpred=session.run([loss,pred],feed_dict={x:vdata[:,:-1],y:vdata[:,-1:]})
+            correct = tf.placeholder(tf.float32,shape=(1,None))
+            predicted = tf.placeholder(tf.float32,shape=(1,None))
+            correct_prediction = tf.equal(correct, predicted)
+            accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+            acc=session.run(accuracy, feed_dict={correct:vdata[:,-1],predicted:pred[:]})
+            print("Validation Loss:{}\tValidation Accurracy".format(vloss,acc))
+            
         
-        """  for i in range(no_of_batches):
-            batch_
-            loss_val,_ =sess.run([loss,optimizer],feed_dict={x:batch_x,y:batch_y}) """
-    
-        #y_pred_batch = session.run(y_pred, {x: x_batch})
-    session.close()
         
 if __name__=="__main__":
     main()
